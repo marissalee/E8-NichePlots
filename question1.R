e78a508b1035a434025d58e62cc1effec654ebdf
Marissa Lee <marissaruthlee@gmail.com>
2014-08-08 23:32:49 -0400

    Fixing pval functions

--- question1.R
+++ question1.R
@@ -39,46 +39,88 @@ sv<-cbind(sdt2[,c(1,3:16)], yrplotname=sdt2[,18], vdt2[,7:11], total=vdt2[,16])
 View(sv)
 
 #########################################################
-#Plot: invader biomass vs native subplot resource availability... color points by year
+#Invader biomass vs native subplot resource availability... color points by year
+
 library(ggplot2); theme_set(theme_bw())
-str(sv)
-sv$year<-as.factor(sv$year)
 
-soil_shortname<-c('noi','toti','nitrifd','minzd','soilmoi')
-soil_longname<-c('Nitrate (ug/G) in Reference Plot','Total Inorganic N (ug/G) in Reference Plot','Nitrification (ug/G*d) in Reference Plot','Mineralization (ug/G*d) in Reference Plot','Soil Moisture (%) in Reference Plot')
+#reshape
+colnames(sv)
+sv1<-sv[,c(1,8,9,11,12,13,17)]
+library(reshape2)
+sv2<-melt(sv1, measure.var=c('noi','toti','nitrifd','minzd','soilmoi'))
+sv2$year<-as.factor(sv2$year)
 
-i<-0
-for (i in 1:length(soil_shortname)){
-  p<-ggplot(sv,aes(sv[,soil_shortname[i]], y = mv)) + geom_point(aes(color=year)) +
-    geom_smooth(aes())+
-    xlab(soil_longname[i]) + ylab("Microstegium biomass (g)")
-  print(p)
-}
+#rename variables
+sv2s <- sv2
+levels(sv2s$variable)[levels(sv2s$variable)=="noi"] <- "Nitrate (ug/G)"
+levels(sv2s$variable)[levels(sv2s$variable)=="toti"] <- "Total Inorganic N (ug/G)"
+levels(sv2s$variable)[levels(sv2s$variable)=="nitrifd"] <- "Nitrification (ug/G*d)"
+levels(sv2s$variable)[levels(sv2s$variable)=="minzd"] <- "Mineralization (ug/G*d)"
+levels(sv2s$variable)[levels(sv2s$variable)=="soilmoi"] <- "Soil Moisture (%)"
 
-i<-0
-store<-numeric(0)
-for (i in 1:length(soil_shortname)){
-  x<-sv[,soil_shortname[i]]
-  y<-sv[,'mv']
-  year<-sv[,'year']
-  fit<-lm(y~x*year)
+s<-ggplot(sv2s,aes(sv2s, x=value, y = mv)) + 
+  geom_point(aes(color=year)) +
+  facet_wrap(~variable+year, scales='free_x', ncol=2) +
+  xlab("Reference plot value") + ylab("Microstegium biomass (g)") +
+  geom_smooth(method='lm',se=F)
+s
+
+
+##########################
+#linear model
+colnames(sv1)
+sv12<-sv1[year==2012,]
+sv13<-sv1[year==2013,]
+
+#Fxns to use within loop
+GetPvals<-function(fit){
+  store<-numeric(0)
+  
   pvalx<-summary(fit)$coefficients[2,4]
-  pvalyear<-summary(fit)$coefficients[3,4]
-  pvalint<-summary(fit)$coefficients[4,4]
   r2<-summary(fit)$r.squared
-  row<-c(pvalx, pvalyear, pvalint,r2)
+  
+  row<-c(pvalx, r2)
   store<-rbind(store,row)
+  colnames(store)<-c('pvalx','r2')
+  
+  return(store)
 }
-colnames(store)<-c('pvalx','pvalyear','pvalint','r2')
-rownames(store)<-soil_shortname
-store
+GetCoefs<-function(fit){
+  store<-numeric(0)
+  
+  int<-summary(fit)$coefficients[1,1]
+  coefx<-summary(fit)$coefficients[2,1]
+  
+  row<-c(int, coefx)
+  store<-rbind(store,row)
+  colnames(store)<-c('int', 'coefx')
+  
+  return(store)
+}
 
+#Fxn to calc pvals and coefs
+AnotherFxn<-function(data){
+  xvars<-colnames(data)[2:6]
+  store<-numeric(0)
+  i<-0
+  for (i in 1:length(xvars)){
+    x<-sv1[,i+1]
+    y<-sv1[,'mv']
+    df<-data.frame(x,y)
+    fit<-lm(y ~ x, df)
+    pvals<-GetPvals(fit)
+    coefs<-GetCoefs(fit)
+    row<-c(pvals,coefs)
+    store<-rbind(store,row)
+  }
+  return(store)
+}
 
+#Do it!
+result12<-AnotherFxn(sv12)
+colnames(result12)<-c('pvalx','r2','int', 'coefx')
+result12
 
-
-
-
-
-
-
-
+result13<-AnotherFxn(sv13)
+colnames(result13)<-c('pvalx','r2','int', 'coefx')
+result13
